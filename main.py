160289a6f9362a27987485ebb9c7e7199e6a8202
SKRYMr <alessio.boiardi@fastwebnet.it>
2024-04-15 15:45:31 +0200

    Finished first merge

--- main.py
+++ main.py
@@ -4,43 +4,75 @@ import sys
 
 RATINGS_DAT_FILE = "./data/ratings.dat"
 MOVIES_DAT_FILE = "./data/movies.dat"
+USERS_DAT_FILE = "./data/users.dat"
 
-RATINGS_CSV_FILE = "./data/ratings.csv"
-MOVIES_CSV_FILE = "./data/movies.csv"
+class MovieLens():
+    def __init__(self):
+        self.ratings = pd.read_table(RATINGS_DAT_FILE, engine="python",sep="::", usecols=[0,1,2],names=["user_id", "movie_id", "rating"])
+        self.movies = pd.read_table(MOVIES_DAT_FILE, engine="python",sep="::", names=["movie_id", "title", "genres"], encoding="latin")
+        self.users = set(pd.read_table(USERS_DAT_FILE, engine="python", sep="::", usecols=[0], names=["user_id"])["user_id"].unique())
+    
+    def get_user_ratings(self, user_id: int) -> pd.DataFrame:
+        user_ratings = self.rating_data[self.rating_data["user_id"] == user_id]
+        return user_ratings
+    
+    def get_user_movies(self, user_id: int) -> pd.DataFrame:
+        return pd.merge(self.get_user_ratings(user_id), database.movies, on="movie_id")
 
-def show_user_movies(user_movies, nmovies=15):
-
-    if nmovies > 15:
-        print("Showing maximum 15 movies")
-        nmovies = 15
-    else:
-        print(f"Showing {nmovies} movies")
-    print("Title - Genres")
-    for _, row in user_movies.iterrows():
-        genres = row['genres'].replace("|", ", ")
-        print(f"{row['title']} - {genres}")
-        nmovies -= 1
-        if nmovies == 0:
-            break
-    return
-
-def check_user_id(user_id: str, df_users: pd.DataFrame) -> int:
-    try:
-        user_id = int(user_id)
-
-        user_df = df_users[df_users["user_id"] == user_id]
+    def get_movie_info(self, movie_id: int) -> pd.DataFrame:
+        movie_info = self.movie_data[self.movie_data["movie_id"] == movie_id]
+        return movie_info
+    
+    def get_user_mean(self, user_id):
+        user_ratings = self.get_user_ratings(user_id)['rating']
+        if user_ratings.empty:
+            return None
+        return user_ratings.mean()
+    
+    def get_common_ratings(self, user_id_1, user_id_2):
+        ratings_user_1 = self.get_user_ratings(user_id_1)
+        ratings_user_2 = self.get_user_ratings(user_id_2)
         
-        if user_df.empty:
-            print("User not found")
-            sys.exit(0)
+        #find common movies between user 1 and user 2
+        common_movie_ids = set(ratings_user_1['movieId']).intersection(set(ratings_user_2['movieId']))
+        
+        common_ratings_user_1 = ratings_user_1[ratings_user_1['movieId'].isin(common_movie_ids)]['rating']
+        common_ratings_user_2 = ratings_user_2[ratings_user_2['movieId'].isin(common_movie_ids)]['rating']
+        
+        return common_ratings_user_1.values, common_ratings_user_2.values
+    
+    def get_rated_movies_ids(self, user_id):
+        return set(self.get_user_ratings(user_id)['movieId'])
+    
+    def get_rated_movies(self, user_id):
+        return pd.merge(self.get_user_ratings(user_id), self.movie_data, on="movieId")
+    
+    def get_unique_users(self):
+        return self.rating_data['userId'].unique()
+    
+    def get_neighbour_movies(self,n):
+        return self.rating_data[self.rating_data.user_id.isin(n)]
 
-        return user_id
-    except ValueError:
-        print("user id must be a number")
-    except Exception as e:
-        print(e)
+def show_user_movies(user_movies: pd.DataFrame, n_movies: int = 15):
 
-    sys.exit(0)
+    if n_movies > 15:
+        n_movies = 15
+
+    print(f"\n{n_movies if len(user_movies) > n_movies else len(user_movies)} Movies Rated By User =========================")
+    for i, row in user_movies.iterrows():
+        # genres = row["genres"].replace("|", ", ")
+        print(f"{i + 1}) {row['title']} - {', '.join(row['genres'])}")
+        n_movies -= 1
+        if n_movies == 0:
+            break
+    print(f"================================================\n")
+
+def check_user_id(user_id: int, users: set) -> bool:
+    if user_id not in users:
+        print("User not found.")
+        print(f"Maximum user ID: {max(users)}")
+        return False
+    return True
 
 def pearson_correlation(col1, col2):
     if len(col1) == 0 or len(col1) < 5:
@@ -56,55 +88,47 @@ def pearson_correlation(col1, col2):
     return abs(a/((b*c)**0.5))
 
 
-def find_k_nearest(user_id, k):
+def find_k_nearest(user_id: int, users: set, ratings: pd.DataFrame, k: int) -> list:
     similarities = {}
-    user_ids = ratings['user_id'].unique()
-    for id in user_ids:
-        if int(id) == int(user_id):
-            continue
+    user_ratings = ratings[ratings.user_id == int(user_id)]
+    for id in users - {user_id}:
         if int(user_id) == 2000:
             break
-        user_ratings1 = ratings[ratings.user_id == int(id)]
-        user_ratings_ab = pd.merge(ratings[ratings.user_id == int(user_id)], user_ratings1, on="movie_id")
-        similarities[id] = pearson_correlation(user_ratings_ab['rating_x'], user_ratings_ab['rating_y'])
+        other_user_ratings = ratings[ratings.user_id == int(id)]
+        user_ratings_ab = pd.merge(user_ratings, other_user_ratings, on="movie_id")
+        similarities[id] = pearson_correlation(user_ratings_ab["rating_x"], user_ratings_ab["rating_y"])
     similarities_sorted = sorted(similarities.items(), key=lambda item: item[1], reverse=True)
     users = [user[0] for user in similarities_sorted[:k]]
     return users
 
-def get_top_movies(user_id, neighbours):
-    user_ratings = ratings[ratings.user_id == int(user_id)]
+def get_top_movies(user_id: int, neighbours: list, ratings: pd.DataFrame, movies: pd.DataFrame):
+    user_ratings = database.get_user_ratings(user_id)
     neighbours_ratings = ratings[ratings.user_id.isin(neighbours)]
     neighbours_ratings = neighbours_ratings[~neighbours_ratings.movie_id.isin(user_ratings.movie_id)]
-    neighbours_ratings = neighbours_ratings.groupby('movie_id').rating.mean().reset_index()
-    neighbours_ratings.sort_values('rating', ascending=False, inplace=True)
+    neighbours_ratings = neighbours_ratings.groupby("movie_id").rating.mean().reset_index()
+    neighbours_ratings.sort_values("rating", ascending=False, inplace=True)
     neighbours_ratings = neighbours_ratings.head(10)
     neighbours_movies = pd.merge(neighbours_ratings, movies, on="movie_id")
     print(f"Top 10 recommended movies for user {user_id}")
-    return show_user_movies(neighbours_movies,10)
+    return show_user_movies(neighbours_movies, 10)
 
 
-if __name__ == '__main__':
-    user_id = input('User id: ')
-    print(user_id)
+if __name__ == "__main__":
+    try:
+        user_id = int(input("User id: "))
+    except ValueError as err:
+        print("User ID must be a number.")
+        sys.exit(0)
 
-    if os.path.exists(RATINGS_CSV_FILE) and os.path.exists(MOVIES_CSV_FILE):
-        ratings = pd.read_csv(RATINGS_CSV_FILE)
-        movies = pd.read_csv(MOVIES_CSV_FILE)
-    else:
-        ratings = pd.read_table(RATINGS_DAT_FILE, engine="python",sep="::", usecols=[0,1,2],names=["user_id", "movie_id", "rating"])
-        movies = pd.read_table(MOVIES_DAT_FILE, engine="python",sep="::", names=["movie_id", "title", "genres"], encoding = 'latin')
+    database = MovieLens()
 
-        ratings.to_csv(RATINGS_CSV_FILE, index=False)
-        movies.to_csv(MOVIES_CSV_FILE, index=False)
+    if not check_user_id(user_id, database.users):
+        sys.exit(0)
 
-    user_ratings = ratings[ratings.user_id == int(user_id)]
-    user_movies = pd.merge(user_ratings, movies, on="movie_id")
+    show_user_movies(database.get_user_movies, n_movies=15)
 
-    show_user_movies(user_movies, nmovies=15)
-
-    neighbours = find_k_nearest(user_id, 5)
+    neighbours = find_k_nearest(user_id, database.users, database.ratings, 5)
     
     print(neighbours)
 
-    get_top_movies(user_id, neighbours)
-
+    get_top_movies(user_id, neighbours, database.ratings, database.movies)
